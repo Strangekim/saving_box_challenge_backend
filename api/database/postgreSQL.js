4e926b2a971b4d5cadc7706a11553994b7c65b1f
Strangekim <114288288+Strangekim@users.noreply.github.com>
2025-08-20 19:43:03 +0900

    폴더 구조 정리, DDL, default insert 정리 (#3)
    Co-authored-by: yjsong2154 <yjsong2153@gmail.com>

---
+++ api/database/postgreSQL.js
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+
+const Pool = pkg
+
+export const client = new Pool({
+  user: process.env.PGSQL_USER,
+  password: process.env.PGSQL_PW,
+  host: process.env.PGSQL_HOST,
+  database: process.env.PGSQL_DB,
+  port: process.env.PGSQL_PORT,
+  max: 10,
+});
+
+client.connect();
