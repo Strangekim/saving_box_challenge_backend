2155501a5d37a74e4577f1cad7c502ebaa8e7d9a
yjsong2154 <yjsong2153@gmail.com>
2025-08-20 18:00:36 +0900

    폴더 구조 정리, DDL, default insert 정리

---
+++ api/util/trycatchWrapper.js
@@ -0,0 +1,9 @@
+export const trycatchWrapper = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (e) {
+      next(e);
+    }
+  };
+};
